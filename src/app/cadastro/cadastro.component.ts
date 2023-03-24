a00c7cffa59ac22a5c907e9d9d4459ea54eb4c15
Francisco Jhonny <francisco.jhonny@hotmail.com>
2023-03-23 20:10:20 -0400

    first commit

---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,44 @@
+import { ConsultaCepService } from "./../service/consulta-cep.service";
+import { Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+@Component({
+  selector: "app-cadastro",
+  templateUrl: "./cadastro.component.html",
+  styleUrls: ["./cadastro.component.css"],
+})
+export class CadastroComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private consultaCepService: ConsultaCepService
+  ) {}
+
+  ngOnInit(): void {}
+
+  cadastrar(form: NgForm) {
+    if (form.valid) this.router.navigate(["sucesso"]);
+    else alert("Formulário inválido");
+  }
+
+  consultaCep(ev: any, f: NgForm) {
+    const cep = ev.target.value;
+    if (cep !== "") {
+      this.consultaCepService.getConsultaCep(cep).subscribe((resultado) => {
+        console.log(resultado);
+        this.populandoEndereco(resultado, f);
+      });
+    }
+  }
+
+  populandoEndereco(dados: any, f: NgForm) {
+    f.form.patchValue({
+      endereco: dados.logradouro,
+      complemento: dados.complemento,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      estado: dados.uf,
+    });
+    console.log(f);
+  }
+}
